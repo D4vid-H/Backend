17bb752771f0de7e475b40c101b63e381c508c19
David Hillton <davidhillton@gmail.com>
2022-08-11 05:58:49 -0300

    Desafio funciona

---
+++ src/Controllers/middelwareController.js
@@ -0,0 +1,9 @@
+function pass (req, res, next){
+    if(req.session?.user){
+        return next();
+    }else{
+        return res.redirect('/login');
+    }    
+}
+
+export {pass};
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,17 @@
 import Router from 'express';
-import getProducts from '../faker/faker.js';
+import getProducts from '../Controllers/faker.js';
+import {pass} from '../Controllers/middelwareController.js';
+import { getIndex, getLogout, getUser, redirect, postLogin, getHome } from '../Controllers/sessionController.js';
 
 const router = Router();
 
 router.get('/productos-test', getProducts);
+router.get('/', redirect);
+
+router.get('/user', getUser)
+router.get('/home', pass, getHome);
+router.get('/login', getIndex);
+router.post('/login', postLogin)
+router.get('/logout', getLogout);
 
 export default router;
